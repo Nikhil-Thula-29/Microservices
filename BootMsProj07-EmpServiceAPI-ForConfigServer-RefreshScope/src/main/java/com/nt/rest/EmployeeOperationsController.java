f12936768f182a95d522e00ccfebcb808fc96ebf
Nikhil-Thula-29 <nikhil.thula29@gmail.com>
2026-05-03 00:55:28 +0530

    refresh scope

---
+++ BootMsProj07-EmpServiceAPI-ForConfigServer-RefreshScope/src/main/java/com/nt/rest/EmployeeOperationsController.java
@@ -0,0 +1,27 @@
+package com.nt.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee-api")
+@RefreshScope //to get the updated values in external config server without restarting the server.
+public class EmployeeOperationsController {
+	
+	@Value("${db.user}")
+	private String username;
+	
+	@Value("${db.pwd}")
+	private String password;
+	
+	@GetMapping("/show")
+	public ResponseEntity<String> showData(){
+		return new ResponseEntity<String>("DB Properties:: "+username+"..."+password,HttpStatus.OK);
+	}
+
+}
